a5713a62a4b5cb3feb2609ef17424eb1a4ae384c
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-02-13 20:10:39 -0500

    Added basic routing to an about page. Added header skeleton.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Header from './components/Header'
+import About from './components/pages/About'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div clasName="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                Library App
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <h1>This is an about page</h1>
+            </header>
+        )
+    }
+}
+
+export default About;
